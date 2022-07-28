69df72276c0a9587b79d70f86f374fb522353eb3
twiby <t.balenbois@gmail.com>
2022-07-28 12:08:46 +0200

    constant speed cinematic model in cartesian coordinates

--- main.py
+++ main.py
@@ -15,23 +15,35 @@ def circle_animated():
 
 	th = 0
 	w = 2
-	state = np.array([th, w])
 
+	##### x = r cost
+	##### dxdt = -r w sint
+	##### y = r sint
+	##### dydt = r w cost
+
+	##### vx^2 + vy^2 = r^2 * w^2
+
+	##### dvxdt = -r w^2 cost
+	##### dvydt = -r w^2 sint
+
+	x0 = CENTER[0] + RADIUS * np.cos(th)
+	y0 = CENTER[1] + RADIUS * np.sin(th)
+	vx0 = - RADIUS * w * np.sin(th)
+	vy0 = RADIUS * w * np.cos(th)
+	state = np.array([x0, y0, vx0, vy0])
 	def derivs(state, t):
 		dxdy = np.zeros_like(state)
-		dxdy[0] = state[1]
-		dxdy[1] = 0.5
+		dxdy[0] = state[2]
+		dxdy[1] = state[3]
+
+		w = np.sqrt(state[2]**2 + state[3]**2) / RADIUS
+		dxdy[2] = - RADIUS * w * w * (state[0] - CENTER[0]) / RADIUS
+		dxdy[3] = - RADIUS * w * w * (state[1] - CENTER[1]) / RADIUS
 		return dxdy
 
 	y = integrate.odeint(derivs, state, t)
-	x1 = CENTER[0] + RADIUS*np.cos(y[:, 0])
-	y1 = CENTER[1] + RADIUS*np.sin(y[:, 0])
 
-
-
-
-
-	animate_movement(x1, y1, dt)
+	animate_movement(y[:, 0], y[:, 1], dt)
 
 if __name__=="__main__":
 	circle_animated()
