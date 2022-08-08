79c864e3711ff2b92900bd7c24c77997019fad54
twiby <t.balenbois@gmail.com>
2022-08-08 17:00:53 +0200

    main: add an argument parser

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import numpy as np
-import scipy.integrate as integrate
+#!/usr/bin/env python
+import argparse
 
 from animate import Animator
 from gravitational_system import GravitationalSystem
 
-if __name__=="__main__":
+def main(args):
+	n = args.bodies
+	dt = args.timestep
+	t_stop = args.duration
+
 	A = Animator()
-	A.animate_movement(GravitationalSystem(5).run(dt = 60*60*24/4, t_stop = 60*60*24*365))
+	A.animate_movement(GravitationalSystem(n).run(dt, t_stop))
 	A.show_animations()
+
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description="Simulate and animate a gravitational system")
+	parser.add_argument('-b','--bodies', type=int, help="number of bodies in the simulation", default=3)
+	parser.add_argument('-t','--timestep', type=float, help="time step of the simulation, in seconds", default=60*60*24/4)
+	parser.add_argument('-d','--duration', type=float, help="duration of the simulation, in seconds", default=60*60*24*365)
+	args = parser.parse_args()
+	main(args)
